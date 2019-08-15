23011453f6846c14c742d63a77d5676b9d3c3c14
John Chapman <chapmjs@gmail.com>
2019-08-14 20:00:24 -0400

    problem setup

---
+++ exercise2.7.1.R
@@ -0,0 +1,16 @@
+library(shiny)
+
+ui <- fluidPage(
+  sliderInput(inputId = "x",
+              label = "If x is",
+              min = 1, max = 50,
+              value = 30),
+  "then, x multipled by 5 is",
+  textOutput("product")
+)
+
+server <- function(input, output, session) {
+  output$product <- renderText({
+    x * 5
+  })
+}
